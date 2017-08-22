dc7b00802e810bd25140bd65c3c7a0f513d4d4bc
admin admin <evaladmin@example.com>
2017-08-22 18:41:55 -0400

    Wrote content /scripts/classes/org/craftercms/aws/AWSHelper.groovy

---
+++ scripts/classes/org/craftercms/aws/AWSHelper.groovy
@@ -0,0 +1,15 @@
+package org.craftercms.aws
+
+class AWSHelper {
+
+    public static doStuff() {
+	
+    	def a =1
+        def b =2
+        def d = 10
+
+        def c = a + b + 10
+
+        return "Hello World " + c
+    }
+}
