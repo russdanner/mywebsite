cba7f5d9a7859cea72ab1139aaa8a74bdfb4a4b2
admin admin <evaladmin@example.com>
2017-09-05 18:50:09 -0400

    Wrote content /scripts/rest/test.get.groovy

--- scripts/rest/test.get.groovy
+++
@@ -1,9 +0,0 @@
-
-def BTCResponseTxt = new URL("https://spotusd-data.btcc.com/data/pro/ticker?symbol=BTCUSD").text
-
-def slurper = new groovy.json.JsonSlurper()
-
-def result = slurper.parseText(BTCResponseTxt)
-
-
-return result
