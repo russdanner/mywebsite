62bf0d1b3e1f8991a88665e33029a512048cd58c
admin admin <evaladmin@example.com>
2017-09-05 18:50:35 -0400

    Wrote content /scripts/components/ticekt.groovy

---
+++ scripts/components/ticekt.groovy
@@ -0,0 +1,9 @@
+
+def BTCResponseTxt = new URL("https://spotusd-data.btcc.com/data/pro/ticker?symbol=BTCUSD").text
+
+def slurper = new groovy.json.JsonSlurper()
+
+def result = slurper.parseText(BTCResponseTxt)
+
+
+templateModel.ticker = result.ticker
