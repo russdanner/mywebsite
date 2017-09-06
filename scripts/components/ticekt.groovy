bd9ff10abbe5ff49d063349330296a36896f9c7d
admin admin <evaladmin@example.com>
2017-09-05 22:08:50 -0400

    Wrote content /scripts/components/ticekt.groovy

--- scripts/components/ticekt.groovy
+++ scripts/components/ticekt.groovy
@@ -1,8 +1,9 @@
 
-def BTCResponseTxt = new URL("https://api.coindesk.com/v1/bpi/historical/close.json").text
+def responseTxt = new URL("https://api.coindesk.com/v1/bpi/historical/close.json").text
 
 def slurper = new groovy.json.JsonSlurper()
 
-def result = slurper.parseText(BTCResponseTxt)
+def result = slurper.parseText(responseTxt)
 
+templateModel.data = responseTxt
 templateModel.dataPoints = result.bpi
