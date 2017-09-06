cccfaf99c2b422381ae064076f0f80706de3987f
Russ Danner <russ.danner@craftersoftware.com>
2017-09-06 19:45:22 -0400

    manual

--- scripts/components/ticker.groovy
+++ scripts/components/ticker.groovy
@@ -1,7 +1,6 @@
 
-def responseTxt = new URL("https://api.coindesk.com/v1/bpi/historical/close.json").text
-
 def slurper = new groovy.json.JsonSlurper()
+def responseTxt = new URL("https://api.coindesk.com/v1/bpi/historical/close.json").text
 
 def result = slurper.parseText(responseTxt)
 
